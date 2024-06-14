45f9eb68fedfa4994975f429057f0ddef569e5bf
root <root@ip-172-31-0-123.ap-south-1.compute.internal>
2024-06-14 17:18:09 +0000

    Intial commit

---
+++ 100/src/main/java/200/application/module/bus/BusService.java
@@ -0,0 +1,10 @@
+package 200.application.module.bus;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@RefreshScope
+@Component
+public class BusService {
+	
+}
